c6b0b66b5ab1c1240b1d3a23bea2f214cbc2dd41
cwadani75 <cwadani75@gmail.com>
2025-06-18 14:43:16 +0300

    labs-inheritance

--- lib/teacher.py
+++ lib/teacher.py
@@ -2,9 +2,14 @@
 
 from user import User
 
-import random
-
 class Teacher(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = [
+            "Functions are reusable blocks of code",
+            "Classes help you structure code",
+            "Everything in Python is an object"
+        ]  # ✅ must be a list with length > 0
 
     def teach(self):
-        pass
+        return self.knowledge[0]  # You can randomize this if needed
